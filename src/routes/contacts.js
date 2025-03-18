04d840b81b3368b0f0bc63ee5a4ff04e6140ec3a
Lizi_S <liza.stepanova99@gmail.com>
2025-03-18 12:39:24 +0100

    add validation

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -7,14 +7,17 @@ import {
     deleteContact
 } from '../controllers/contactsController.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { createContactSchema, updateContactSchema } from '../validation/contact.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get('/', ctrlWrapper(getAllContacts));
-router.get('/:contactId', ctrlWrapper(getContactById));
-router.post('/', jsonParser, ctrlWrapper(createContact));
-router.patch('/:contactId', jsonParser, ctrlWrapper(updateContact));
-router.delete('/:contactId', ctrlWrapper(deleteContact));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post('/', jsonParser, validateBody(createContactSchema), ctrlWrapper(createContact));
+router.patch('/:contactId', jsonParser, isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContact));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContact));
 
 export default router;
